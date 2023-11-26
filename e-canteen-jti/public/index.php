46078d672802ea3775e3dc350f6b7a69b7e6a256
zharsuke <alazharisqi@gmail.com>
2023-11-26 17:13:43 +0700

    feat: add login process route with post method

--- e-canteen-jti/public/index.php
+++ e-canteen-jti/public/index.php
@@ -2,11 +2,18 @@
 
 require_once '../app/app/Router.php';
 require_once '../app/controllers/LoginController.php';
+require_once '../app/controllers/AdminController.php';
+require_once '../app/controllers/CashierController.php';
 use app\Router;
 use controllers\LoginController;
+use controllers\AdminController;
+use controllers\CashierController;
 
 $route = new Router();
-$route->add('GET', '/', LoginController::class, 'index');
+$route->add('GET', '/login', LoginController::class, 'login');
+$route->add('POST', '/login', LoginController::class, 'loginProcess');
+$route->add('GET', '/admin/home', AdminController::class, 'adminHome');
+$route->add('GET', '/cashier/home', CashierController::class, 'cashierHome');
 $route->run();
 
 ?>
