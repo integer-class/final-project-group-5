3f1cec0108819771b73c3a08e8ad0f2fbef79728
zharsuke <alazharisqi@gmail.com>
2023-11-26 17:15:47 +0700

    feat: create login auth

--- e-canteen-jti/app/controllers/LoginController.php
+++ e-canteen-jti/app/controllers/LoginController.php
@@ -1,10 +1,28 @@
 <?php 
 
 namespace controllers;
+require_once '../app/models/UserModel.php';
+use models\UserModel;
 
 class LoginController {
-    public function index() {
+    public function login() {
         require_once '../app/views/auth/login.php';
+    }
+
+    public function loginProcess() {
+        $userModel = new UserModel();
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+        $userData = $userModel->getUserData($username, $password);
+        $role = $userData['role'];
+        
+        if ($role === 'admin') {
+            header('Location: /admin/home');
+        } else if ($role  === 'cashier') {
+            header('Location: /cashier/home');
+        } else {
+            header('Location: /login');
+        }
     }
 }
 
