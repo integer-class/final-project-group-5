4a952562ab794eae454be6ac7a0f4db0ed927e97
zharsuke <alazharisqi@gmail.com>
2023-11-26 17:14:16 +0700

    feat: add login form to test

--- e-canteen-jti/app/views/auth/login.php
+++ e-canteen-jti/app/views/auth/login.php
@@ -6,6 +6,32 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>Ini login</h1>
+    <header>
+        <h1>Login Page</h1><br>
+    </header>
+
+    <main>
+    <form action="/login" method="POST">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username" size="20"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password" size="20"></td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input type="submit" name="Login" value="Proses"></td>
+            </tr>
+        </table>
+    </form>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 - E-Canteen JTI</p>
+    </footer>
+
 </body>
 </html>
