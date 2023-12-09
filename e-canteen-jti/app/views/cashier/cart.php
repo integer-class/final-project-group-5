b02e77694f67008737b4c2a1bc58f9997ba941f4
zharsuke <alazharisqi@gmail.com>
2023-12-10 03:24:14 +0700

    feat: haa

--- e-canteen-jti/app/views/cashier/cart.php
+++ e-canteen-jti/app/views/cashier/cart.php
@@ -14,14 +14,15 @@ include 'templates/navbar.php';
 
 
 <div class="cart">
-    <h2>Cart</h2><br>
+    <h2>Products</h2><br>
     <div class="product-list">
         <?php foreach ($product_data as $product) : ?>
             <div class="product">
                 <img src="/uploads/<?php echo basename($product['image']); ?>" alt="<?php echo $product['product_name']; ?>">
                 <h3><?php echo $product['product_name']; ?></h3>
-                <p>Rp<?php echo $product['sell_price']; ?></p>
-                <p><?php echo $product['stock']; ?> Stocks Available</p>
+                <p>Price: Rp <?php echo number_format($product['sell_price'], 0, ',', '.'); ?></p>
+                <p>Stocks: <?php echo $product['stock']; ?></p>
+                <a href="#">Add to Cart</a>
             </div>
         <?php endforeach; ?>
     </div>
