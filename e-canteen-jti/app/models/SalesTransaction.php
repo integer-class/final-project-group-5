38c3aa98bc1c0f89b39ab8563b9031cd8eaca102
zharsuke <alazharisqi@gmail.com>
2023-12-10 23:14:13 +0700

    feat: add SalesTransaction and SalesTransactionDetail models

---
+++ e-canteen-jti/app/models/SalesTransaction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace models;
+require_once '../app/config/Connection.php';
+use config\Connection;
+require_once 'MasterData.php';
+use models\MasterData;
+
+class SalesTransaction extends MasterData {
+    private $sales_transaction_id;
+    private $sales_transaction_date;
+    private $total;
+    private $paid;
+    private $change;
+    private $user_id;
+    private $connect;
+
+    public function __construct() {
+        $connection = new Connection();
+        $this->connect = $connection->connect;
+    }
+
+    public function getAll() {
+        $query = "SELECT * FROM SalesTransaction";
+        $result = mysqli_query($this->connect, $query);
+        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $row;
+    }
+
+    public function create($data) {
+        $query = "INSERT INTO Sales (sales_transaction_id, sales_transaction_date, total, paid, change, user_id)
+        SELECT ?, CONVERT_TZ(NOW(), '+00:00', '+07:00'), 
+               (SELECT SUM(subtotal) FROM SalesTransactionDetail WHERE sales_transaction_id = ?) AS total, 
+               ?, ?, ?";
+        $statement = mysqli_prepare($this->connect, $query);    
+        mysqli_stmt_bind_param($statement, 'sdddd', $data['sales_transaction_id'], $data['sales_transaction_id'], $data['paid'], $data['change'], $data['user_id']);
+    
+        $result = mysqli_stmt_execute($statement);
+        return $result;
+    }
+    
+   
+}
+
+?>
---
+++ e-canteen-jti/app/models/SalesTransactionDetail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace models;
+require_once '../app/config/Connection.php';
+use config\Connection;
+require_once 'MasterData.php';
+use models\MasterData;
+
+class SalesTransactionDetail extends MasterData {
+    private $sales_transaction_detail_id;
+    private $sales_transaction_id;
+    private $product_id;
+    private $quantity;
+    private $subtotal;
+    private $connect;
+
+    public function __construct() {
+        $connection = new Connection();
+        $this->connect = $connection->connect;
+    }
+
+    public function getAll() {
+        $query = "SELECT * FROM SalesTransactionDetail";
+        $result = mysqli_query($this->connect, $query);
+        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $row;
+    }
+
+    public function create($data) {
+        $query = "INSERT INTO SalesTransactionDetail (sales_transaction_id, product_id, quantity, unit_price, subtotal)
+                    SELECT 
+                        ? AS sales_transaction_id,
+                        ? AS product_id,
+                        ? AS quantity,
+                        p.sell_price AS unit_price,
+                        ? * p.sell_price AS subtotal
+                    FROM Product AS p
+                    WHERE p.product_id = ?";
+        $statement = mysqli_prepare($this->connect, $query);
+        mysqli_stmt_bind_param($statement, 'iiidi', $data['sales_transaction_id'], $data['product_id'], $data['quantity'], $data['quantity'], $data['product_id']);
+        $result = mysqli_stmt_execute($statement);
+        return $result;
+    }
+    
+
+}
+
+?>
